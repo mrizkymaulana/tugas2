ae5c297d3c2d4b8741ac8437c4a65492d913a0bf
mrizkymaulana <muhamadrizkym11@gmail.com>
2016-10-01 14:05:02 +0700

    update index.php

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php 
 	echo "10114119 - Muhamad Rizky Maulana";
-
-<<<<<<< HEAD
-	
-	
 	echo "10114114 - Dimar Radhian";
-=======
 	echo "10114110 - Idza Runianti Darusman";
->>>>>>> bebd428f37a4f53bfdf787aa7acd85494d5aa2c1
+
